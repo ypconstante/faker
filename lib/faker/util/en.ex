d6e9568c133f98c3b5bd03ea936328666113be96
Yuri Pereira Constante <ypconstante@gmail.com>
2024-06-25 00:32:41 -0300

    Remove dependency between Util and Util.En

--- lib/faker/util/en.ex
+++ lib/faker/util/en.ex
@@ -1,44 +1,138 @@
 defmodule Faker.Util.En do
   @moduledoc false
 
-  import Faker.Util, only: [pick: 1]
-
-  @digit ~w/0 1 2 3 4 5 6 7 8 9/
-  @lowercase_alphabet ~w/a b c d e f g h i j k l m n o p q r s t u v w x y z/
-  @uppercase_alphabet ~w/A B C D E F G H I J K L M N O P Q R S T U V W X Y Z/
-  @alphabet @lowercase_alphabet ++ @uppercase_alphabet
+  import Faker, only: [sampler: 2]
 
   @doc """
   Get a random digit as a string; one of 0-9
   """
   @spec digit() :: binary
-  def digit do
-    pick(@digit)
-  end
+  sampler(:digit, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
 
   @doc """
   Get a random alphabet character as a string; one of a-z or A-Z
   """
   @spec letter() :: binary
-  def letter do
-    pick(@alphabet)
-  end
+  sampler(:letter, [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ])
 
   @doc """
   Get a random lowercase character as a string; one of a-z
   """
   @spec lower_letter() :: binary
-  def lower_letter do
-    pick(@lowercase_alphabet)
-  end
+  sampler(:lower_letter, [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ])
 
   @doc """
   Get a random uppercase character as a string; one of A-Z
   """
   @spec upper_letter() :: binary
-  def upper_letter do
-    pick(@uppercase_alphabet)
-  end
+  sampler(:upper_letter, [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ])
 
   @doc """
   Converts a list to a string, with "and" before the last item. Uses an Oxford comma.
